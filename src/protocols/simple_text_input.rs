e9a318211cd937f654c9ff781e3af879fa2bc5d0
Tom Gundersen <teg@jklm.no>
2019-02-05 19:56:34 +0100

    protocols/simple_text_input: derive Default for InputKey
    Signed-off-by: Tom Gundersen <teg@jklm.no>

--- src/protocols/simple_text_input.rs
+++ src/protocols/simple_text_input.rs
@@ -9,7 +9,7 @@ pub const PROTOCOL_GUID: crate::base::Guid = crate::base::Guid::from_fields(
 );
 
 #[repr(C)]
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Default)]
 pub struct InputKey {
     pub scan_code: u16,
     pub unicode_char: crate::base::Char16,
