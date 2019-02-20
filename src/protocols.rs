aa3515e7a85c20d662342f9ad15aff518fca81f4
David Rheinsberg <david.rheinsberg@gmail.com>
2019-02-20 10:24:36 +0100

    r-efi: minor coding-style update
    Make sure to list protocols alphabetically.
    
    Signed-off-by: David Rheinsberg <david.rheinsberg@gmail.com>

--- src/protocols.rs
+++ src/protocols.rs
@@ -4,6 +4,6 @@
 //! refer to each other, but their documentation and implementation is split apart. We provide
 //! each protocol as a separate module, so it is clearly defined where a symbol belongs to.
 
-pub mod simple_text_input_ex;
 pub mod simple_text_input;
+pub mod simple_text_input_ex;
 pub mod simple_text_output;
