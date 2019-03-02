3744917dd008bf3ec7b9a18e320d1110eb6dd497
Richard Wiedenhöft <richard@wiedenhoeft.xyz>
2019-03-02 18:36:26 +0100

    protocols: Add graphics output protocol
    Implementation of EFI_GRAPHICS_OUTPUT_PROTOCOL structure as
    described in the UEFI specification version 2.7.

--- src/protocols.rs
+++ src/protocols.rs
@@ -8,6 +8,7 @@ pub mod decompress;
 pub mod device_path;
 pub mod device_path_utilities;
 pub mod file;
+pub mod graphics_output;
 pub mod loaded_image;
 pub mod loaded_image_device_path;
 pub mod simple_file_system;
