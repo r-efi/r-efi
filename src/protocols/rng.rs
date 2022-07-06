d02b31e446cfda18f2a73e9495d665ac290c35db
David Rheinsberg <david.rheinsberg@gmail.com>
2022-07-06 13:10:13 +0200

    protocols/rng: adjust formatting
    Minor adjustments to the recent RNG protocol addition:
    
     * Keep the `_GUID` suffix for GUID constants just as the spec defines
       them.
    
     * Fix a GUID typo in ALGORITHM_SP800_90_HMAC_256_GUID.
    
     * Prefix protocol member types with `Protocol*`.
    
    Signed-off-by: David Rheinsberg <david.rheinsberg@gmail.com>

--- src/protocols/rng.rs
+++ src/protocols/rng.rs
@@ -1,6 +1,7 @@
 //! Random Number Generator Protocol
 //!
-//! This protocol is used to provide random numbers for use in applications, or entropy for seeding other random number generators.
+//! This protocol is used to provide random numbers for use in applications, or
+//! entropy for seeding other random number generators.
 
 pub const PROTOCOL_GUID: crate::base::Guid = crate::base::Guid::from_fields(
     0x3152bca5,
@@ -13,7 +14,7 @@ pub const PROTOCOL_GUID: crate::base::Guid = crate::base::Guid::from_fields(
 
 pub type Algorithm = crate::base::Guid;
 
-pub const ALGORITHM_SP800_90_HASH_256: Algorithm = crate::base::Guid::from_fields(
+pub const ALGORITHM_SP800_90_HASH_256_GUID: Algorithm = crate::base::Guid::from_fields(
     0xa7af67cb,
     0x603b,
     0x4d42,
@@ -21,15 +22,15 @@ pub const ALGORITHM_SP800_90_HASH_256: Algorithm = crate::base::Guid::from_field
     0x21,
     &[0x70, 0xbf, 0xb6, 0x29, 0x3f, 0x96],
 );
-pub const ALGORITHM_SP800_90_HMAC_256: Algorithm = crate::base::Guid::from_fields(
-    0xc5159b43,
+pub const ALGORITHM_SP800_90_HMAC_256_GUID: Algorithm = crate::base::Guid::from_fields(
+    0xc5149b43,
     0xae85,
     0x4f53,
     0x99,
     0x82,
     &[0xb9, 0x43, 0x35, 0xd3, 0xa9, 0xe7],
 );
-pub const ALGORITHM_SP800_90_CTR_256: Algorithm = crate::base::Guid::from_fields(
+pub const ALGORITHM_SP800_90_CTR_256_GUID: Algorithm = crate::base::Guid::from_fields(
     0x44f0de6e,
     0x4d8c,
     0x4045,
@@ -37,7 +38,7 @@ pub const ALGORITHM_SP800_90_CTR_256: Algorithm = crate::base::Guid::from_fields
     0xc7,
     &[0x4d, 0xd1, 0x68, 0x85, 0x6b, 0x9e],
 );
-pub const ALGORITHM_X9_31_3DES: Algorithm = crate::base::Guid::from_fields(
+pub const ALGORITHM_X9_31_3DES_GUID: Algorithm = crate::base::Guid::from_fields(
     0x63c4785a,
     0xca34,
     0x4012,
@@ -45,7 +46,7 @@ pub const ALGORITHM_X9_31_3DES: Algorithm = crate::base::Guid::from_fields(
     0xc8,
     &[0x0b, 0x6a, 0x32, 0x4f, 0x55, 0x46],
 );
-pub const ALGORITHM_X9_31_AES: Algorithm = crate::base::Guid::from_fields(
+pub const ALGORITHM_X9_31_AES_GUID: Algorithm = crate::base::Guid::from_fields(
     0xacd03321,
     0x777e,
     0x4d3d,
@@ -62,13 +63,13 @@ pub const ALGORITHM_RAW: Algorithm = crate::base::Guid::from_fields(
     &[0x7f, 0xfd, 0xc4, 0xb6, 0x85, 0x61],
 );
 
-pub type GetInfo = eficall! {fn(
+pub type ProtocolGetInfo = eficall! {fn(
     *mut Protocol,
     *mut usize,
     *mut Algorithm,
 ) -> crate::base::Status};
 
-pub type GetRNG = eficall! {fn(
+pub type ProtocolGetRng = eficall! {fn(
     *mut Protocol,
     *mut Algorithm,
     usize,
@@ -77,6 +78,6 @@ pub type GetRNG = eficall! {fn(
 
 #[repr(C)]
 pub struct Protocol {
-    get_info: GetInfo,
-    get_rng: GetRNG,
+    get_info: ProtocolGetInfo,
+    get_rng: ProtocolGetRng,
 }
