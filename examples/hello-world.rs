bef3c0af798d354877a371caac5672cef4216aff
David Rheinsberg <david.rheinsberg@gmail.com>
2022-07-26 14:08:57 +0200

    examples: add freestanding uefi application
    Add a freestanding UEFI application example, showing how to compile for
    UEFI targets without any dependencies. Refer to this from hello-world.rs
    to make sure we do not duplicate documentation on panic-handlers and
    entry-point setup.
    
    Signed-off-by: David Rheinsberg <david.rheinsberg@gmail.com>

--- examples/hello-world.rs
+++ examples/hello-world.rs
@@ -1,26 +1,18 @@
 // Example: Hello World!
 //
-// This is an example UEFI application that prints "Hello World!", then waits for key input before
-// it exits. It serves as base example how to write UEFI applications without any helper modules
-// other than the UEFI protocol definitions.
+// This is an example UEFI application that prints "Hello World!", then waits
+// for key input before it exits. It serves as base example how to write UEFI
+// applications without any helper modules other than the UEFI protocol
+// definitions.
 //
-// The `main` function serves as entry-point. Depending on your target-configuration, it must be
-// exported with a pre-configured name so the linker will correctly mark it as entry-point. The
-// target configurations shipped with upstream rust-lang use `efi_main` as symbol name.
+// This example builds upon the `freestanding.rs` example, using the same setup
+// and rust integration. See there for details on the panic-handler and entry
+// point configuration.
 //
-// Additionally, a panic handler is provided. This is executed by rust on panic. For simplicity,
-// we simply end up in an infinite loop. For real applications, this method should probably call
-// into `SystemTable->boot_services->exit()` to exit the UEFI application. Note, however, that
-// UEFI applications are likely to run in the same address space as the entire firmware. Hence,
-// halting the machine might be a viable alternative. All that is out-of-scope for this example,
-// though.
-//
-// Lastly, note that UEFI uses UTF-16 strings. Since rust literals are UTF-8, we have to use an
-// open-coded, zero-terminated, UTF-16 array as argument to `output_string()`. Similarly to the
-// panic handler, real applications should rather use UTF-16 modules.
-//
-// Note that as of rust-1.31.0, all features used here are stabilized. No unstable features are
-// required, nor do we rely on nightly compilers.
+// Note that UEFI uses UTF-16 strings. Since rust literals are UTF-8, we have
+// to use an open-coded, zero-terminated, UTF-16 array as argument to
+// `output_string()`. Similarly to the panic handler, real applications should
+// rather use UTF-16 modules.
 
 #![no_main]
 #![no_std]
