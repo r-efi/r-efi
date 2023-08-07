b6c10e3747715ceb72e99a562e6f78100130d9ef
David Rheinsberg <david@readahead.eu>
2023-08-07 14:32:20 +0200

    system: include extern configuration table guids
    Include the Guids listed in the UEFI specification, used by external
    industry standards to store references in the UEFI configuration table.
    
    Signed-off-by: David Rheinsberg <david@readahead.eu>

--- src/system.rs
+++ src/system.rs
@@ -434,6 +434,102 @@ pub struct MemoryAttributesTable<const N: usize = 0> {
 }
 
 //
+// External Configuration Tables
+//
+// This lists the Guids of configuration tables of other industry standards, as listed in
+// the UEFI specification. See each standard for details on the data included in each table.
+//
+
+pub const ACPI_10_TABLE_GUID: crate::base::Guid = crate::base::Guid::from_fields(
+    0xeb9d2d30,
+    0x2d88,
+    0x11d3,
+    0x9a,
+    0x16,
+    &[0x00, 0x90, 0x27, 0x3f, 0xc1, 0x4d],
+);
+
+pub const ACPI_20_TABLE_GUID: crate::base::Guid = crate::base::Guid::from_fields(
+    0x8868e871,
+    0xe4f1,
+    0x11d3,
+    0xbc,
+    0x22,
+    &[0x00, 0x80, 0xc7, 0x3c, 0x88, 0x81],
+);
+
+pub const DTB_TABLE_GUID: crate::base::Guid = crate::base::Guid::from_fields(
+    0xb1b621d5,
+    0xf19c,
+    0x41a5,
+    0x83,
+    0x0b,
+    &[0xd9, 0x15, 0x2c, 0x69, 0xaa, 0xe0],
+);
+
+pub const JSON_CAPSULE_DATA_TABLE_GUID: crate::base::Guid = crate::base::Guid::from_fields(
+    0x35e7a725,
+    0x8dd2,
+    0x4cac,
+    0x80,
+    0x11,
+    &[0x33, 0xcd, 0xa8, 0x10, 0x90, 0x56],
+);
+
+pub const JSON_CAPSULE_RESULT_TABLE_GUID: crate::base::Guid = crate::base::Guid::from_fields(
+    0xdbc461c3,
+    0xb3de,
+    0x422a,
+    0xb9, 0xb4,
+    &[0x98, 0x86, 0xfd, 0x49, 0xa1, 0xe5],
+);
+
+pub const JSON_CONFIG_DATA_TABLE_GUID: crate::base::Guid = crate::base::Guid::from_fields(
+    0x87367f87,
+    0x1119,
+    0x41ce,
+    0xaa,
+    0xec,
+    &[0x8b, 0xe0, 0x11, 0x1f, 0x55, 0x8a],
+);
+
+pub const MPS_TABLE_GUID: crate::base::Guid = crate::base::Guid::from_fields(
+    0xeb9d2d2f,
+    0x2d88,
+    0x11d3,
+    0x9a,
+    0x16,
+    &[0x00, 0x90, 0x27, 0x3f, 0xc1, 0x4d],
+);
+
+pub const SAL_SYSTEM_TABLE_GUID: crate::base::Guid = crate::base::Guid::from_fields(
+    0xeb9d2d32,
+    0x2d88,
+    0x11d3,
+    0x9a,
+    0x16,
+    &[0x00, 0x90, 0x27, 0x3f, 0xc1, 0x4d],
+);
+
+pub const SMBIOS_TABLE_GUID: crate::base::Guid = crate::base::Guid::from_fields(
+    0xeb9d2d31,
+    0x2d88,
+    0x11d3,
+    0x9a,
+    0x16,
+    &[0x00, 0x90, 0x27, 0x3f, 0xc1, 0x4d],
+);
+
+pub const SMBIOS3_TABLE_GUID: crate::base::Guid = crate::base::Guid::from_fields(
+    0xf2fd1544,
+    0x9794,
+    0x4a2c,
+    0x99,
+    0x2e,
+    &[0xe5, 0xbb, 0xcf, 0x20, 0xe3, 0x94],
+);
+
+//
 // Global Tables
 //
 // UEFI uses no global state, so all access to UEFI internal state is done through vtables you get
