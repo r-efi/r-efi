1b9c4b2bca924f2755b6cb72e3881cd6919edd03
David Rheinsberg <david@readahead.eu>
2023-08-07 14:41:27 +0200

    system: include RT_PROPERTIES and CONFORMANCE_PROFILES tables
    Include the definitions of the configuration tables RT_PROPERTIES and
    CONFORMANCE_PROFILES, which are the remaining configuration tables from
    the spec which we have not yet defined.
    
    Signed-off-by: David Rheinsberg <david@readahead.eu>

--- src/system.rs
+++ src/system.rs
@@ -391,6 +391,40 @@ pub struct ConfigurationTable {
     pub vendor_table: *mut core::ffi::c_void,
 }
 
+pub const RT_PROPERTIES_TABLE_GUID: crate::base::Guid = crate::base::Guid::from_fields(
+    0xeb66918a,
+    0x7eef,
+    0x402a,
+    0x84,
+    0x2e,
+    &[0x93, 0x1d, 0x21, 0xc3, 0x8a, 0xe9],
+);
+
+pub const RT_PROPERTIES_TABLE_VERSION: u16 = 0x0001;
+
+pub const RT_SUPPORTED_GET_TIME: u32 = 0x0000001;
+pub const RT_SUPPORTED_SET_TIME: u32 = 0x0000002;
+pub const RT_SUPPORTED_GET_WAKEUP_TIME: u32 = 0x0000004;
+pub const RT_SUPPORTED_SET_WAKEUP_TIME: u32 = 0x0000008;
+pub const RT_SUPPORTED_GET_VARIABLE: u32 = 0x00000010;
+pub const RT_SUPPORTED_GET_NEXT_VARIABLE_NAME: u32 = 0x00000020;
+pub const RT_SUPPORTED_SET_VARIABLE: u32 = 0x00000040;
+pub const RT_SUPPORTED_SET_VIRTUAL_ADDRESS_MAP: u32 = 0x00000080;
+pub const RT_SUPPORTED_CONVERT_POINTER: u32 = 0x00000100;
+pub const RT_SUPPORTED_GET_NEXT_HIGH_MONOTONIC_COUNT: u32 = 0x00000200;
+pub const RT_SUPPORTED_RESET_SYSTEM: u32 = 0x00000400;
+pub const RT_SUPPORTED_UPDATE_CAPSULE: u32 = 0x00000800;
+pub const RT_SUPPORTED_QUERY_CAPSULE_CAPABILITIES: u32 = 0x00001000;
+pub const RT_SUPPORTED_QUERY_VARIABLE_INFO: u32 = 0x00002000;
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug)]
+pub struct RtPropertiesTable {
+    pub version: u16,
+    pub length: u16,
+    pub runtime_services_supported: u32,
+}
+
 pub const PROPERTIES_TABLE_GUID: crate::base::Guid = crate::base::Guid::from_fields(
     0x880aaca3,
     0x4adc,
@@ -431,6 +465,34 @@ pub struct MemoryAttributesTable<const N: usize = 0> {
     pub descriptor_size: u32,
     pub reserved: u32,
     pub entry: [MemoryDescriptor; N],
+}
+
+pub const CONFORMANCE_PROFILES_TABLE_GUID: crate::base::Guid = crate::base::Guid::from_fields(
+    0x36122546,
+    0xf7e7,
+    0x4c8f,
+    0xbd,
+    0x9b,
+    &[0xeb, 0x85, 0x25, 0xb5, 0x0c, 0x0b],
+);
+
+pub const CONFORMANCE_PROFILES_TABLE_VERSION: u16 = 0x0001;
+
+pub const CONFORMANCE_PROFILES_UEFI_SPEC_GUID: crate::base::Guid = crate::base::Guid::from_fields(
+    0x523c91af,
+    0xa195,
+    0x4382,
+    0x81,
+    0x8d,
+    &[0x29, 0x5f, 0xe4, 0x00, 0x64, 0x65],
+);
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug)]
+pub struct ConformanceProfilesTable<const N: usize = 0> {
+    pub version: u16,
+    pub number_of_profiles: u16,
+    pub conformance_profiles: [crate::base::Guid; N],
 }
 
 //
