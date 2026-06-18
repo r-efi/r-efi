42de7c377449c13dfa4c4ef31222fad0bf177edf
David Rheinsberg <david@readahead.eu>
2026-06-18 12:00:31 +0200

    proto/partition_info: fix revision number
    Fix the revision number to align with the spec.
    
    Signed-off-by: David Rheinsberg <david@readahead.eu>

--- src/protocols/partition_info.rs
+++ src/protocols/partition_info.rs
@@ -13,7 +13,7 @@ pub const PROTOCOL_GUID: Guid = Guid::from_fields(
     &[0xec, 0x9e, 0xc4, 0x21, 0xa1, 0xa0],
 );
 
-pub const REVISION: u32 = 0x00010000;
+pub const REVISION: u32 = 0x00001000;
 
 pub const TYPE_OTHER: u32 = 0x00;
 pub const TYPE_MBR: u32 = 0x01;
