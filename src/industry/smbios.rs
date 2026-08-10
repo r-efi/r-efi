4e7535ccbf13f8c1e1050854ea6553fcc05877f8
David Rheinsberg <david@readahead.eu>
2026-08-10 10:01:40 +0200

    industry/smbios: add historical string max length
    Add STRING_MAX_LENGTH with the maximum length of SMBIOS text strings.
    This was a historical limit, newer editions no longer have this
    restriction.
    
    Signed-off-by: David Rheinsberg <david@readahead.eu>

--- src/industry/smbios.rs
+++ src/industry/smbios.rs
@@ -9,3 +9,10 @@
 ///
 /// Reference SMBIOS 2.7, chapter 6.1.2.
 pub const HANDLE_PI_RESERVED: u16 = 0xfffe;
+
+/// Old SMBIOS editions had a hard limit on the length of a string. This symbol
+/// defines the historical maximum length of _significant characters_ in an
+/// SMBIOS text string. No such limitation exists in newer editions.
+///
+/// Reference SMBIOS 2.6, chapter 3.1.3. Removed in newer editions.
+pub const STRING_MAX_LENGTH: usize = 64;
